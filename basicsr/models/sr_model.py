a95ca7d7efa85c5a74e9571d0b5384c04af33742
Anh Nguyen <anhlone3@gmail.com>
2026-09-20 13:42:23 +0700

    feat: add training configurations and model implementation for ECBSR and EDSR

--- basicsr/models/sr_model.py
+++ basicsr/models/sr_model.py
@@ -205,17 +205,21 @@ class SRModel(BaseModel):
             self.test()
 
             visuals = self.get_current_visuals()
-            sr_img = tensor2img([visuals['result']])
-            metric_data['img'] = sr_img
-            if 'gt' in visuals:
-                gt_img = tensor2img([visuals['gt']])
-                metric_data['img2'] = gt_img
-                del self.gt
+            if save_img:
+                sr_img = tensor2img([visuals['result']])
+                metric_data['img'] = sr_img
+                if 'gt' in visuals:
+                    gt_img = tensor2img([visuals['gt']])
+                    metric_data['img2'] = gt_img
+            else:
+                metric_data['img'] = visuals['result']
+                if 'gt' in visuals:
+                    metric_data['img2'] = visuals['gt']
 
-            # tentative for out of GPU memory
+            if 'gt' in visuals:
+                del self.gt
             del self.lq
             del self.output
-            torch.cuda.empty_cache()
 
             if save_img:
                 if self.opt['is_train']:
